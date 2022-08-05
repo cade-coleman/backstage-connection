8f5f2c66f10b25d6295cef87a2a205e7f75c6474
Cade Coleman <105037672+cade-coleman@users.noreply.github.com>
2022-08-05 11:40:18 -0500

    files added

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const routes = require('./controllers')
+const routes = require('./controllers/api')
 
 const sequelize = require('./config/connection')
 const app = express();
